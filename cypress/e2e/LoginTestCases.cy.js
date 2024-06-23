fb111d4c80b0ef15b9ba1c9020eb0666c3f667ac
amreidkok <eng.amrhassan1013@gmail.com>
2024-06-23 05:00:41 +0300

    Adding before each in the login testcases

--- cypress/e2e/LoginTestCases.cy.js
+++ cypress/e2e/LoginTestCases.cy.js
@@ -1,14 +1,26 @@
 import { LoginPage } from "./Pages/LoginPage"
 import { RegistrationPage } from "./Pages/RegistrationPage"
 
+beforeEach(function()
+{
+  cy.fixture('Registrationtestdata').then(function(regdata)
+  {
+    
+    this.regdata=regdata
+   
+  })
+
+}
+)
+
 
   it('ValidLoginTestCase', function(){
  
 const lgp=new LoginPage()
 const rgp=new RegistrationPage()
 cy.visit('https://parabank.parasoft.com/')
-lgp.InsertUserName('adminamr')
-lgp.InsertPassword('amr')
+lgp.InsertUserName(this.regdata.username)
+lgp.InsertPassword(this.regdata.password)
 lgp.ClickOnLoginBtn()
 lgp.ValidateOnWelcomeText('Welcome')
     
